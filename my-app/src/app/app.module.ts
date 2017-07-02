84937a32e5e44e9a42c01e3de1d5611a69ad5805
jonathannicolas <hello@gmail.com>
2017-07-02 17:44:07 +0200

    Add Http Request

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
@@ -10,11 +12,19 @@ import { HeroService }          from './hero.service';
 
 import { AppRoutingModule }     from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
+
   ],
   declarations: [
     AppComponent,
